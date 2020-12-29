44c8dfcec6e04a19ef78e2de38c97f49f9fcb8d5
Omar Rizwan <omar@omar.website>
2020-12-29 03:03:38 -0800

    test: test test-page.html

--- test/test.c
+++ test/test.c
@@ -1,8 +1,3 @@
-//usr/bin/env cc -o test "$0" && ./test; exit
-
-// (run this file directly with `./test.c` in most shells; if that
-// doesn't work, run it with `sh test.c`)
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -25,6 +20,7 @@ char* expand(char* phrase) { // expand path with wildcard
 
 // integration tests
 int main() {
+    // if you don't have node, comment this out, I guess:
     assert(system("node ../extension/background.js --unhandled-rejections=strict") == 0); // run quick local JS tests
 
     // reload the extension so we know it's the latest code.
@@ -34,11 +30,19 @@ int main() {
 
     assert(file_contents_equal(expand("../fs/mnt/extensions/TabFS*/enabled"), "true"));
 
-    // FIXME: synthesize some kind of web page
-    assert(system("echo about:blank > ../fs/mnt/tabs/create") == 0);
-    // FIXME: race here
-    assert(file_contents_equal("../fs/mnt/tabs/last-focused/url.txt", "about:blank"));
-    assert(system("echo remove > ../fs/mnt/tabs/last-focused/control") == 0);
+    {
+        assert(system("echo about:blank > ../fs/mnt/tabs/create") == 0);
+        // FIXME: race here?
+        assert(file_contents_equal("../fs/mnt/tabs/last-focused/url.txt", "about:blank"));
+        assert(system("echo remove > ../fs/mnt/tabs/last-focused/control") == 0);
+    }
+
+    {
+        assert(system("echo file://$(pwd)/test-page.html > ../fs/mnt/tabs/create") == 0);
+        assert(file_contents_equal("../fs/mnt/tabs/last-focused/title.txt", "Title of Test Page"));
+        assert(file_contents_equal("../fs/mnt/tabs/last-focused/text.txt", "Body Text of Test Page"));
+        assert(system("echo remove > ../fs/mnt/tabs/last-focused/control") == 0);
+    }
 
     assert(1); printf("Done!\n"); 
 }
